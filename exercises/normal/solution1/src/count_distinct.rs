b4769d095c4b0dce5d1cf58214637f2a1de125ac
zenghao.333 <zenghao.333@bytedance.com>
2025-02-26 10:06:11 +0800

    mid-1

--- exercises/normal/solution1/src/count_distinct.rs
+++ exercises/normal/solution1/src/count_distinct.rs
@@ -1,3 +1,7 @@
 pub fn new_count_distinct(input_str: &str) -> usize {
-    todo!()
+    let v = input_str.to_string();
+    let mut v = v.split(",").collect::<Vec<_>>();
+    v.sort();
+    v.dedup();
+    v.len()
 }
