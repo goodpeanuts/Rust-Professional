1ebfd27b51c1e404af28cfaa6977bba01d8918e9
zenghao.333 <zenghao.333@bytedance.com>
2025-02-26 11:47:33 +0800

    mid-4

--- exercises/normal/solution4/src/rec_mc.rs
+++ exercises/normal/solution4/src/rec_mc.rs
@@ -1,4 +1,11 @@
-pub fn dp_rec_mc(amount: u32) -> u32 {
-    // TODO: 这里写逻辑
-    todo!()
+pub fn dp_rec_mc(mut amount: u32) -> u32 {
+    let mut res = 0;
+
+    let ty = [100, 50, 30, 20, 10, 5, 2, 1];
+    ty.iter().for_each(|t| {
+        res += amount / t;
+        amount %= t;
+    });
+
+    res
 }
