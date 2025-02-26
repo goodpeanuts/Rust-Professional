99f5e22b0fcd7542adcfe8a7d08a681d6d8fce78
zenghao.333 <zenghao.333@bytedance.com>
2025-02-26 11:37:11 +0800

    mid-3

--- exercises/normal/solution3/src/calc_logic.rs
+++ exercises/normal/solution3/src/calc_logic.rs
@@ -1,4 +1,7 @@
 pub fn new_birthday_probability(n: u32) -> f64 {
-    // TODO: 这里写逻辑
-    todo!()
+    let mut p = 1.0;
+    for i in 0..n {
+        p *= (365.0 - i as f64) / 365.0;
+    }
+    1.0 - p
 }
